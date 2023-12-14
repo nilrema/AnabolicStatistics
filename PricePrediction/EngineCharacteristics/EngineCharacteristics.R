e4cff14839910cfe18c335b83520ab0cb3a2799b
Andrija Merlin <105887989+nilrema@users.noreply.github.com>
2023-12-14 12:50:19 +0100

    Create EngineCharacteristics.R

---
+++ PricePrediction/EngineCharacteristics/EngineCharacteristics.R
@@ -0,0 +1,21 @@
+# Učitavanje podataka
+data <- read.csv("C:/Users/itsth/Downloads/car_specifications.csv")
+
+data$make <- as.factor(data$make)
+data$aspiration <- as.factor(data$aspiration)
+data$num_of_doors <- as.factor(data$num_of_doors)
+data$body_style <- as.factor(data$body_style)
+data$drive_wheels <- as.factor(data$drive_wheels)
+data$engine_location <- as.factor(data$engine_location)
+data$fuel <- as.factor(data$fuel)
+data$country <- as.factor(data$country)
+data$continent <- as.factor(data$continent)
+
+# sa levels printam sve kategorije ovih varijabli
+#levels(data$make)
+#levels(data$aspiration)
+
+model <- lm(price ~ horsepower + engine_size + num_of_cylinders + fuel_system, data = data)
+
+summary(model)
+plot(model)
